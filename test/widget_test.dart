92e674a3be155330c47a0107584940df38f922d9
GP4cK <gautier.bayzelon@gmail.com>
2023-04-04 19:00:17 +0800

    🧪 added tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:riverpod_family_dependencies/main.dart';
@@ -10,6 +11,64 @@ void main() {
         child: ProviderScope(
           overrides: [a('hello world').overrideWithValue(text)],
           child: const MyApp(),
+        ),
+      ),
+    );
+    expect(find.text(text), findsOneWidget);
+  });
+
+  testWidgets('test scoping family provider with overrideWith', (tester) async {
+    const text = 'hello riverpod';
+    const family = 'hello world';
+    final a =
+        Provider.family.autoDispose<String, String>((ref, value) => value);
+
+    final b = Provider.family.autoDispose<String, String>(
+      (ref, value) => ref.watch(a(value)),
+      dependencies: [a],
+    );
+
+    await tester.pumpWidget(
+      ProviderScope(
+        child: ProviderScope(
+          overrides: [
+            a.overrideWith((ref, value) => text)
+          ], // Overrides `a` entirely.
+          child: Consumer(
+            builder: (_, ref, __) => Text(
+              ref.watch(b(family)),
+              textDirection: TextDirection.ltr,
+            ),
+          ),
+        ),
+      ),
+    );
+    expect(find.text(text), findsOneWidget);
+  });
+
+  testWidgets('test scoping a provider with overrideWith', (tester) async {
+    const text = 'hello riverpod';
+    const family = 'hello world';
+    final a =
+        Provider.family.autoDispose<String, String>((ref, value) => value);
+
+    final b = Provider.family.autoDispose<String, String>(
+      (ref, value) => ref.watch(a(value)),
+      dependencies: [a],
+    );
+
+    await tester.pumpWidget(
+      ProviderScope(
+        child: ProviderScope(
+          overrides: [
+            a(family).overrideWith((ref) => text)
+          ], // Only override a(family).
+          child: Consumer(
+            builder: (_, ref, __) => Text(
+              ref.watch(b(family)),
+              textDirection: TextDirection.ltr,
+            ),
+          ),
         ),
       ),
     );
